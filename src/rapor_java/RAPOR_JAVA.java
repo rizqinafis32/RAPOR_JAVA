18f70e7e4d1d38451cc647d2a0bc9aadeb62d8ee
ASUS <ASUS@LAPTOP-20F42915>
2020-12-13 13:21:21 +0700

    1

---
+++ src/rapor_java/RAPOR_JAVA.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rapor_java;
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ * 
+ */
+public class RAPOR_JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nilaitugas,nilaiulangan,nilaipts,nilaipas;
+        double ratarata;
+        boolean running = true;
+         Scanner input = new Scanner (System.in);       
+        String ulang,nama;
+        
+        
+        
+        
+        
+    }
+    
+}
